2b40370b012ea3c01733e8d9f04d9567b7431a25
WanerValencia2019 <valenciawaner@gmail.com>
2021-02-22 19:10:17 -0500

    change password route

--- auth/change_password.php
+++ auth/change_password.php
@@ -19,6 +19,47 @@ if ($_SERVER['REQUEST_METHOD'] == 'PUT') {
         http_response_code(401);
         echo json_encode($response);
     }
+    function tokenUsernameVerify($connection, $token, $username){
+        $sql_token = 'SELECT pwd, id FROM users AS u INNER JOIN auth_tokens AS auth ON u.id=auth.id_user AND auth.token = ? AND u.username = ?';
+        $consulta = $connection->prepare($sql_token);
+        $consulta -> bind_param('ss',$token,$username);
+        if (!$consulta -> execute()){
+            return [];
+        } else{
+            $result = $consulta -> get_result();
+            $data = $result->fetch_assoc();
+            if(isset($data)) {
+                return [
+                    "hash_pwd"=>$data['pwd'],
+                    'id' => $data['id']
+                ];
+            }else{
+                return [];
+            }
+        }
+
+    }
+    function passwordHashVerify($password, $hash){
+        $veryfied = password_verify($password, $hash);
+        return $veryfied;
+    }
+    function changePassword($connection, $id,$new_password) {
+        $sql = "UPDATE users SET pwd=? WHERE id=?";
+        $hash = password_hash($new_password,PASSWORD_BCRYPT);
+        $consulta=$connection->prepare($sql);
+        $consulta -> bind_param('si',$hash,$id);
+        if (!$consulta -> execute()){
+            paramsError("password cannot be changed");
+            exit();
+        } else{
+            if($consulta->affected_rows == 0){
+                paramsError("password cannot be changed");
+                exit();
+            }else{
+                return true;
+            }
+        }
+    }
     $headers = apache_request_headers();
     $_PUT = json_decode(file_get_contents('php://input'),true);
 
@@ -31,19 +72,38 @@ if ($_SERVER['REQUEST_METHOD'] == 'PUT') {
         exit();
     }
 
-    $username = $_PUT['username'];
-    $password = $_PUT['password'];
-    $password_confirm = $_PUT['password_confirm'];
-
-
     if (isset($headers['Authorization'])) {    
         include './../database/connection.php';
 
+        $username = $_PUT['username'];
+        $old_password = $_PUT['old_password'];
+        $password = $_PUT['password'];
+        $password_confirm = $_PUT['password_confirm'];
         $token = $headers['Authorization'];
         $conn = $connection;
-        $sql="DELETE FROM auth_tokens WHERE token = ?";
 
-
+        $user = tokenUsernameVerify($conn,$token,$username);
+        
+        if(isset($user['hash_pwd'])){
+            $hash = $user['hash_pwd'];
+            $id_user = $user['id'];
+            if(passwordHashVerify($old_password,$hash)){
+                $changed = changePassword($conn, $id_user,$password_confirm);
+                if($changed){
+                    $response = [
+                        "message" => "password changed successfully"
+                    ];
+                    echo json_encode($response);
+                    exit();
+                }
+            }else{
+                paramsError("Old password incorrect");
+                exit();
+            }
+        }else{
+            paramsError("Wrong parameters, verify your token or username,old_password");
+            exit();
+        }
     }else{
         tokenError();
         exit();
